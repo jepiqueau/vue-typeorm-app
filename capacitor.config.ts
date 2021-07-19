7ff40b5e58913f765bef9f8553c263b4489efa42
jepiqueau <jepi.queau@free.fr>
2021-07-19 11:58:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-typeorm-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
